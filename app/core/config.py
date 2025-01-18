0d687abe85bc37a30437e4098262c2eb20cd67c0
Andrew <skkqw@yandex.ru>
2025-01-19 01:24:12 +0300

    Добавлены настройки для проекта

---
+++ app/core/config.py
@@ -0,0 +1,54 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    """
+    Класс настройки.
+    """
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    REDIS_PORT: int
+    REDIS_PASSWORD: str
+    REDIS_HOST: str
+    REDIS_USERNAME: str
+
+    BASE_URL: str = "http://127.0.0.1:8000"
+    BASE_DIR: str = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_DIR: str = os.path.join(BASE_DIR, "uploads")
+    STATIC_DIR: str = os.path.join(BASE_DIR, "static")
+
+    model_config = SettingsConfigDict(
+        env_file=os.path.abspath(os.path.join('.env'))
+    )
+
+
+settings = Settings()
+
+
+def get_db_url():
+    """
+    Генерирует URL для подключения к PostgreSQL базе данных.
+
+    :return: Строка с URL для подключения к базе данных.
+    """
+    return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
+            f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
+
+
+def get_auth_data():
+    """
+    Возвращает данные для аутентификации, необходимые для работы с JWT.
+
+    :return: Словарь с данными для аутентификации.
+    """
+    return {'secret_key': settings.SECRET_KEY, 'algorithm': settings.ALGORITHM}
---
+++ app/core/database.py
@@ -0,0 +1,37 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
+from .config import get_db_url
+
+
+DATABASE_URL = get_db_url()
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    """
+    Абстрактный класс для моделей бд.
+    """
+
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f'{cls.__name__.lower()}s'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), unique=True, nullable=False,
+        comment='Уникальный идентификатор пользователя.'
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), comment='Дата и время создания записи пользователя.'
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now(), comment='Дата и время последнего обновления записи пользователя.'
+    )
