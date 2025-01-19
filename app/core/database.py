1136a010b92e899b54dc334a7c84abca6dd81bb8
Andrew <skkqw@yandex.ru>
2025-01-19 11:35:26 +0300

    Изменения в архитектуре. Добавлен класс для работы с бд DatabaseHelper

--- app/core/database.py
+++ app/core/database.py
@@ -1,17 +1,48 @@
 import uuid
 from datetime import datetime
+from os import write
 
 from sqlalchemy import func
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 
-from .config import get_db_url
+from .config import settings
 
 
-DATABASE_URL = get_db_url()
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+class DatabaseHelper:
+    """
+    Класс для управления базой данных
+    """
+
+    def __init__(
+            self,
+            url: str,
+            echo: bool = False,
+            echo_pool: bool = False,
+            pool_size: int = 5,
+            max_overflow: int = 10
+    ) -> None:
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow
+        )
+        self.async_session_maker = async_sessionmaker(
+            bing=self.engine,
+            expire_on_commit=False,
+            autocommit=False,
+            autoflush=False,
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self):
+        async with self.async_session_maker() as session:
+            yield session
 
 
 class Base(AsyncAttrs, DeclarativeBase):
@@ -35,3 +66,12 @@ class Base(AsyncAttrs, DeclarativeBase):
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now(), comment='Дата и время последнего обновления записи пользователя.'
     )
+
+
+db_halper = DatabaseHelper(
+    url=settings.get_db_url(),
+    echo=settings.ECHO,
+    echo_pool =settings.ECHO_POOL,
+    pool_size=settings.POOL_SIZE,
+    max_overflow=settings.MAX_OVERFLOW,
+)
