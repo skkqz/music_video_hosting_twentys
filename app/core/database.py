6c400073cb7369c4e5bc2d230c8079a7ab460976
Andrew <skkqw@yandex.ru>
2025-01-19 11:40:51 +0300

    Добавлено описание

--- app/core/database.py
+++ app/core/database.py
@@ -12,7 +12,7 @@ from .config import settings
 
 class DatabaseHelper:
     """
-    Класс для управления базой данных
+    Класс для управления подключением к базе данных и создания асинхронных сессий.
     """
 
     def __init__(
@@ -23,6 +23,15 @@ class DatabaseHelper:
             pool_size: int = 5,
             max_overflow: int = 10
     ) -> None:
+        """
+        Инициализирует DatabaseHelper.
+
+        :param url: URL для подключения к базе данных.
+        :param echo: Если True, SQLAlchemy будет выводить все SQL-запросы в консоль.
+        :param echo_pool: Если True, SQLAlchemy будет выводить информацию о пуле соединений.
+        :param pool_size: Количество соединений в пуле.
+        :param max_overflow: Максимальное количество соединений, которые могут быть созданы сверх pool_size.
+        """
         self.engine = create_async_engine(
             url=url,
             echo=echo,
@@ -31,38 +40,57 @@ class DatabaseHelper:
             max_overflow=max_overflow
         )
         self.async_session_maker = async_sessionmaker(
-            bing=self.engine,
+            bind=self.engine,
             expire_on_commit=False,
             autocommit=False,
             autoflush=False,
         )
 
     async def dispose(self) -> None:
+        """
+        Закрывает все соединения с базой данных.
+        """
         await self.engine.dispose()
 
     async def session_getter(self):
+        """
+        Генератор для получения асинхронной сессии.
+
+        :yield: Асинхронная сессия для работы с базой данных.
+        """
         async with self.async_session_maker() as session:
             yield session
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     """
-    Абстрактный класс для моделей бд.
+    Абстрактный базовый класс для всех моделей базы данных.
+
+    Этот класс предоставляет общие поля и методы для всех моделей, такие как:
+    - Автоматическое создание имени таблицы.
+    - Уникальный идентификатор (UUID).
+    - Временные метки создания и обновления.
     """
 
     __abstract__ = True
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
+        """
+        Автоматически генерирует имя таблицы на основе имени класса.
+        :return: Имя таблицы в нижнем регистре с добавлением 's' в конце.
+        """
         return f'{cls.__name__.lower()}s'
 
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False, index=True,
         comment='Уникальный идентификатор пользователя.'
     )
+
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), comment='Дата и время создания записи пользователя.'
     )
+
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now(), comment='Дата и время последнего обновления записи пользователя.'
     )
@@ -71,7 +99,7 @@ class Base(AsyncAttrs, DeclarativeBase):
 db_halper = DatabaseHelper(
     url=settings.get_db_url(),
     echo=settings.ECHO,
-    echo_pool =settings.ECHO_POOL,
+    echo_pool=settings.ECHO_POOL,
     pool_size=settings.POOL_SIZE,
     max_overflow=settings.MAX_OVERFLOW,
 )
