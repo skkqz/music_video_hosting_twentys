bc9e265e1666a8e5ecc40311f8feaaf1349204d4
Andrew <skkqw@yandex.ru>
2025-01-19 10:10:31 +0300

    изменения в базовой модели бд

--- app/core/database.py
+++ app/core/database.py
@@ -26,7 +26,7 @@ class Base(AsyncAttrs, DeclarativeBase):
         return f'{cls.__name__.lower()}s'
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), unique=True, nullable=False,
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False, index=True,
         comment='Уникальный идентификатор пользователя.'
     )
     created_at: Mapped[datetime] = mapped_column(
