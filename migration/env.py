f3fc28d00f42d499cecc312d8792e3c153fb05fd
Andrew <skkqw@yandex.ru>
2025-01-19 23:40:35 +0300

    Изменение импорта

--- migration/env.py
+++ migration/env.py
@@ -12,7 +12,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from app.core.database import Base, settings
+from app.core.database_config import Base, settings
 from app.modules.user.models import User
 
 
